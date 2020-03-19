711690d83f27942e44f34d015a67ae7b18f8734d
Dolidodzik <teethtard321@gmail.com>
2020-03-19 18:02:15 +0000

    [BE, #24] Added last 10 positions display in trip component

--- CyclingBE/core/serializers.py
+++ CyclingBE/core/serializers.py
@@ -8,12 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
-class TripSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Trip
-        fields = ('id', 'owner', 'created_date', 'modified_date', 'is_finished', 'distance', 'time', 'avg_speed', 'max_speed', 'last_pos_lat', 'last_pos_lon', 'number_of_points')
-        read_only_fields = ('created_date', 'modified_date', 'is_finished', 'distance', 'time', 'avg_speed', 'max_speed', 'last_pos_lat', 'last_pos_lon', 'number_of_points')
-
 class PointSerializer(serializers.ModelSerializer):
     class Meta:
         model = Point
@@ -23,3 +17,16 @@ class Point60Serializer(serializers.ModelSerializer):
     class Meta:
         model = Point60
         fields = ('id', 'trip', 'lon', 'lat', 'timestamp', 'was_paused')
+
+class TripSerializer(serializers.ModelSerializer):
+
+    last_10_positions = serializers.SerializerMethodField('last10Positions')
+
+    def last10Positions(self, instance):
+        last_10_positions = Point.objects.filter(trip=instance).order_by('timestamp')[:-10]
+        return PointSerializer(last_10_positions, many=True).data
+
+    class Meta:
+        model = Trip
+        fields = ('id', 'owner', 'created_date', 'modified_date', 'is_finished', 'distance', 'time', 'avg_speed', 'max_speed', 'last_pos_lat', 'last_pos_lon', 'number_of_points', 'last_10_positions')
+        read_only_fields = ('created_date', 'modified_date', 'is_finished', 'distance', 'time', 'avg_speed', 'max_speed', 'last_pos_lat', 'last_pos_lon', 'number_of_points')
